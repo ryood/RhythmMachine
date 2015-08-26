1f7d766a11380d6c475f8afb5e382d855b607554
ryood <kawanabe@gmail.com>
2015-08-26 18:35:25 +0900

    Sequencer_I2C_Slave.c: change to toggle mode

--- Atmel Stduio/Sequencer_I2C_Slave/Sequencer_I2C_Slave/Sequencer_I2C_Slave.c
+++ Atmel Stduio/Sequencer_I2C_Slave/Sequencer_I2C_Slave/Sequencer_I2C_Slave.c
@@ -2,9 +2,9 @@
  * Sequencer_I2C_Slave.c
  *
  * Created: 2015/08/26 17:08:41
-  *  Author: gizmo
+ *  Author: gizmo
  *
- * クロック：内蔵1ＭＨｚ
+ * クロック：内蔵1MHz
  * デバイス：ATmega88V
  * Fuse bit：hFuse:DFh lFuse:62h eFuse:01h
  *
@@ -17,24 +17,25 @@
  *
  * PortD[PD6..PD7]: LEDx2
  * PortD[PB0]     : SWx1
+ *
+ * AtmelStudio 6.2
+ *
  */ 
 
-#define 	F_CPU 1000000UL  // 8 MHz
+#define 	F_CPU 1000000UL  // 1MHz
 
 #include 	<avr/io.h>
 #include	<util/delay.h>
 
-//状態値
+//TWI 状態値
 #define SR_SLA_ACK  0xA8		//SLA_R 受信チェック
 #define	SR_DATA_ACK	0xC0		//送信パケットチェック
 #define	SR_ENDP_ACK	0xA0		//終了orリピートチェック
 
-//proto
-void twi_init(void);
-void twi_send(uint8_t sdata);
-
-//------------------------------------------------
-
+//------------------------------------------------//
+// TWI
+//
+//------------------------------------------------//
 void twi_error(){
 	PORTD = 0b11000000;
 	while(1);
@@ -49,36 +50,6 @@ void twi_init(){
 	
 	//slave address
 	TWAR=0xfe;
-}
-
-int main(){
-	uint8_t sdata;
-
-
-	//sw input / pull up
-	DDRD = 0x00;
-	PORTD = 0b00111111;
-	
-	DDRB = 0x00;
-	PORTB = 0b11000000;
-	
-	//Error LED
-	DDRB |= 0b11000010;
-
-	//PORTB = 0b11000000;
-
-	//通信モジュール初期化
-	twi_init();
-	
-	for(;;) {
-		// swの押し下げ状態を読み取る
-		sdata  = (~PIND & 0b00011111);
-		sdata |= ((~PINB & 0b11000000) >> 1);
-		sdata |= ((~PIND & 0b00100000) << 2);
-		
-		//データを送信する。
-		twi_send(sdata);		
-	}
 }
 
 //データパケットを１バイト送る。
@@ -102,4 +73,44 @@ void twi_send(uint8_t sdata){
 	
 	if((TWSR & 0xF8 ) != SR_DATA_ACK)
 		twi_error();
+}
+
+//------------------------------------------------//
+// Main routine
+//
+//------------------------------------------------//
+int main(){
+	uint8_t sdata;
+	uint8_t prev_sdata = 0x00;
+
+
+	//sw input / pull up
+	DDRD = 0x00;
+	PORTD = 0b00111111;
+	
+	DDRB = 0x00;
+	PORTB = 0b11000000;
+	
+	//Error LED
+	DDRB |= 0b11000010;
+
+	//PORTB = 0b11000000;
+
+	//通信モジュール初期化
+	twi_init();
+	
+	for(;;) {
+		// swの押し下げ状態を読み取る
+		sdata  = (~PIND & 0b00011111);
+		sdata |= ((~PINB & 0b11000000) >> 1);
+		sdata |= ((~PIND & 0b00100000) << 2);
+		
+		// toggle
+		sdata = prev_sdata ^ sdata;
+		
+		//データを送信する。
+		twi_send(sdata);
+		
+		prev_sdata = sdata;
+	}
 }
