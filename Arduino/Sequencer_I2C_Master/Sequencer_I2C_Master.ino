cb9658cdbe5adf31068fd02c5ad762384ba3724e
ryood <kawanabe@gmail.com>
2015-08-30 14:03:47 +0900

    Arduino: Sequencer_I2C_Master.ino: シリアル通信開始時にメッセージを表示

--- Arduino/Sequencer_I2C_Master/Sequencer_I2C_Master.ino
+++ Arduino/Sequencer_I2C_Master/Sequencer_I2C_Master.ino
@@ -24,6 +24,7 @@ void setup()
   
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
+  Serial.println("Read Slave Data...");
 }
 
 void loop()
